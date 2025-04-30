f73410461915821025064ad2a893010954a30536
Crearcos <soporte@crearcos.com>
2025-04-30 17:11:53 -0500

    Merge 2 Christopher

--- app_vendedores_backend/usuarios/urls.py
+++ app_vendedores_backend/usuarios/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import login_view
+from .views import register_user
 
 urlpatterns = [
     path('login/', login_view, name='login'),
+    path('register/', register_user, name='register_user'),
 ]
--- app_vendedores_backend/usuarios/views.py
+++ app_vendedores_backend/usuarios/views.py
@@ -1,4 +1,6 @@
 # Create your views here.
+import random
+import string
 from django.contrib.auth.models import User
 from usuarios.models import UserProfile
 from django.http import JsonResponse
@@ -38,5 +40,35 @@ def login_view(request):
             return JsonResponse({"status": 1, "message": "Bienvenido Vendedor"})
 
         return JsonResponse({"status": -1, "message": "Rol desconocido"})  # Si el rol no es válido
+
+    return JsonResponse({"error": "Método no permitido"}, status=405)
+
+def generate_random_password(length=10):
+    """Genera una contraseña aleatoria."""
+    characters = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(random.choice(characters) for i in range(length))
+
+@csrf_exempt
+def register_user(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        email = data.get('email')
+        role = data.get('role')
+
+        # Verificar si el usuario ya existe
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({"status": -1, "message": "El correo ya está registrado"})
+
+        # Generar una contraseña aleatoria
+        random_password = generate_random_password()
+        print(f"Contraseña generada para {email}: {random_password}")  # Imprimir en la terminal
+
+        # Crear usuario en auth_user
+        user = User.objects.create_user(username=email, email=email, password=random_password)
+
+        # Asignar rol en usuarios_userprofile
+        UserProfile.objects.create(user=user, role=role)
+
+        return JsonResponse({"status": 0, "message": "Usuario registrado exitosamente"})
 
     return JsonResponse({"error": "Método no permitido"}, status=405)
