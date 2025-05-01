b578e66164aab63ae299c1b9033e7abf97bf6cb4
Crearcos <soporte@crearcos.com>
2025-04-30 22:06:06 -0500

    Merge Alexandra 1

--- app_vendedores_frontend/lib/vendedor/vendedorMain.dart
+++ app_vendedores_frontend/lib/vendedor/vendedorMain.dart
@@ -1,3 +1,4 @@
+import 'package:app_vendedores_frontend/vendedor//registrar_empresa.dart';
 import 'package:flutter/material.dart';
 
 class SellerScreen extends StatelessWidget {
@@ -7,8 +8,25 @@ class SellerScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Vendedor')),
-      body: const Center(
-        child: Text('Vendedor', style: TextStyle(fontSize: 24)),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Vendedor', style: TextStyle(fontSize: 24)),
+            const SizedBox(height: 16),
+
+            // Botón para ir a la pantalla de registro de empresa
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const RegisterUserScreen()),
+                );
+              },
+              child: const Text("Registrar nueva empresa"),
+            ),
+          ],
+        ),
       ),
     );
   }
